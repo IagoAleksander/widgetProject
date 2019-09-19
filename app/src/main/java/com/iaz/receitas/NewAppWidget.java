50cdc8a048dfd6071060ed1c35025522b1ad8db6
Iago Aleksander <iago.zanardo@monitoratec.com.br>
2019-09-19 01:44:32 -0300

    Solução para a customização de um widget recem-criado

--- app/src/main/java/com/iaz/receitas/NewAppWidget.java
+++ app/src/main/java/com/iaz/receitas/NewAppWidget.java
@@ -4,7 +4,10 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.iaz.receitas.presentation.ui.activities.RecipeActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -20,15 +23,18 @@ public class NewAppWidget extends AppWidgetProvider {
 //        TODO (3): Como removemos o textView do layout do widget, não é mais
 //         necessária sua atualização. Assim, devemos remove-lo.
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
 //        TODO (4): Habilitar a abertura do aplicativo através da
 //         - criaçao de um Intent para a mainActivity
 //         - encapsulamento desse intent por um PendingIntent
 //         - ligação de uma view com o PendingIntent criado.
+
+        Intent intent = new Intent(context, RecipeActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
