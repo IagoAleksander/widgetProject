0cb9d1fd77fb9e7b7660d31cee911b98add5e54f
Iago Aleksander <iago.zanardo@monitoratec.com.br>
2019-09-19 01:28:25 -0300

    Exercicio para a customização de um widget recem-criado

---
+++ app/src/main/java/com/iaz/receitas/NewAppWidget.java
@@ -0,0 +1,57 @@
+package com.iaz.receitas;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+
+
+public class NewAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+
+//        TODO (3): Como removemos o textView do layout do widget, não é mais
+//         necessária sua atualização. Assim, devemos remove-lo.
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+//        TODO (4): Habilitar a abertura do aplicativo através da
+//         - criaçao de um Intent para a mainActivity
+//         - encapsulamento desse intent por um PendingIntent
+//         - ligação de uma view com o PendingIntent criado.
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
